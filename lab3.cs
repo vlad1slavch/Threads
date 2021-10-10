4390039219fe97d79dabf531b3384f162c07516d
Vladislav Chuprina <69348111+vlad1slavch@users.noreply.github.com>
2021-10-10 20:47:48 +0300

    Add files via upload

---
+++ lab3.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _lab3
+{
+    public partial class Form1 : Form
+    {
+        int[] arr;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void PrintArray()
+        {
+            g.Clear(Color.Black);
+            for (int i = 0; i < 80; i++)
+            {
+                g.FillRectangle(new SolidBrush(Color.White), i*10, panel1.Height - arr[i], 5, panel1.Height);  
+            }
+        }
+        private void reset_btn_Click(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+            int NumEntries = 80;
+            int MaxVal = panel1.Height;
+            arr = new int[NumEntries];
+
+            g.FillRectangle(new SolidBrush(Color.Black), 0, 0, panel1.Width, MaxVal);          
+            Random r = new Random();
+
+            for (int i = 0; i < NumEntries; i++)
+            {
+                arr[i] = r.Next(0, MaxVal);
+            }
+
+            for (int i = 0; i < NumEntries; i++)
+            {
+                //g.FillRectangle(new System.Drawing.SolidBrush(Color.White), i, MaxVal - arr[i], 1, MaxVal);
+                g.FillRectangle(new SolidBrush(Color.White), i*10, MaxVal - arr[i], 5, MaxVal);                
+            }
+
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            iSortEngine se = new Sort();
+
+            //Thread t1 = new Thread(() => { se.asc(arr, g, panel1.Height); });
+            //Thread t2 = new Thread(() => { se.desc(arr, g, panel1.Height); });
+            //Thread t3 = new Thread(() => { se.Draw(); });//
+
+            ////t1.Priority = ThreadPriority.Normal;
+            ////t2.Priority = ThreadPriority.Normal;
+            ////t3.Priority = ThreadPriority.Highest;
+            // t1.Start();
+            //t1.Join();
+
+            //t2.Start();
+            //t2.Join();
+
+            //t3.Start();
+            //t3.Join();
+
+            Parallel.Invoke(() => se.asc(arr, g, panel1.Height),        //ascending sorting
+                            () => se.desc(arr, g, panel1.Height),       //descending sorting
+                            () => { Thread.Sleep(10); se.Draw(); });    //drawing array
+
+            Thread.Sleep(10);
+            PrintArray();
+        }
+    }
+}
---
+++ lab3_sort.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+
+namespace _lab3
+{
+    class Sort : iSortEngine
+    {
+        //private object o = null;
+        private bool _sorted = false;
+        private int[] arr;
+        private Graphics g;
+        private int MaxVal;
+        //Brush WhiteBrush = new SolidBrush(Color.White);
+        //Brush BlackBrush = new SolidBrush(Color.Black);
+        public void asc(int[] arr_In, Graphics g_In, int MaxVal_In)
+        {
+            arr = arr_In;
+            g = g_In;
+            MaxVal = MaxVal_In;
+
+            while(!_sorted)
+            {
+                for (int i = 0; i < arr.Count() - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        //Swap(i, i + 1);
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        //
+                        //o = new int[] { i, i+1 };
+                        Thread.Sleep(1);
+                    }
+                }
+                _sorted = IsSorted();
+            }
+        }
+
+        public void desc(int[] arr_In, Graphics g_In, int MaxVal_In)
+        {
+            arr = arr_In;
+            g = g_In;
+            MaxVal = MaxVal_In;
+
+            while (!_sorted)
+            {
+                for (int i = arr.Count() - 1; i > 0; i--)
+                {
+                    if (arr[i] < arr[i - 1])
+                    {
+                        //Swap(i, i - 1);
+                        int temp = arr[i];
+                        arr[i] = arr[i - 1];
+                        arr[i - 1] = temp;
+                        //
+                        //o = new int[] { i, i - 1 };
+                        Thread.Sleep(1);
+                    }
+                }
+                _sorted = IsSorted();               
+            }
+        }
+
+        public void Draw() 
+        {
+            while (!_sorted)
+            {
+                g.Clear(Color.Black);
+                for (int i = 0; i < 80; i++)
+                {
+                    g.FillRectangle(new SolidBrush(Color.White), i * 10, MaxVal - arr[i], 5, MaxVal);   
+                }
+                Thread.Sleep(50);
+            }
+        }
+        private bool IsSorted()
+        {
+            for (int i = 0; i < arr.Count() - 1; i++)
+            {
+                if (arr[i] > arr[i + 1]) return false;
+            }
+            return true;
+        }
+    }
+}
+
+//if (o != null)
+//{
+//    int i = ((int[])o)[0];
+//    int p = ((int[])o)[1];
+
+//    g.FillRectangle(BlackBrush, i*10, 0, 5, MaxVal);
+//    g.FillRectangle(BlackBrush, p*10, 0, 5, MaxVal);
+
+//    g.FillRectangle(WhiteBrush, i*10, MaxVal - arr[i], 5, MaxVal);
+//    g.FillRectangle(WhiteBrush, p*10, MaxVal - arr[p], 5, MaxVal);
+
+//    o = null;
+//    Thread.Sleep(10);
+//}
